6362978f9a28b6862b9f26425f4c84058da63f64
Ajirthan <ajirbala28@gmail.com>
2023-04-18 23:16:00 +0530

    Added variables.ts (Completed Section 01/19 of the course)

---
+++ variables.ts
@@ -0,0 +1,43 @@
+let movieTitle: string = "Amadeus";
+movieTitle = "Arrival";
+movieTitle = 9;
+movieTitle.toUpperCase();
+
+let numCatLives: number = 9;
+numCatLives += 1;
+// numCatLives = "zero";
+
+let gameOver: boolean = false;
+gameOver = true;
+gameOver = "true";
+
+null;
+undefined;
+
+let nothing: null = null;
+let foo: undefined = undefined;
+
+
+// Type Inference
+let tvShow = "Olive Kitteridge";
+tvShow = "The Other Two";
+tvShow = false;
+
+let isFunny = false;
+isFunny = true;
+isFunny = "asd";
+
+// the any type
+// let thing: any = "hello";
+// thing = 1;
+// thing = false;
+// thing();
+// thing.toUpperCase();
+const movies = ["Arrival", "The Thing", "Aliens", "Amadeus"];
+let foundMovie: string;
+
+for (let movie of movies) {
+    if (movie === "Amadeus") {
+        foundMovie = "Amadeus";
+    }
+}
