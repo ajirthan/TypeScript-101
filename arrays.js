d3339c975927b5c0183979b7f5003ce7143df671
Ajirthan <ajirbala28@gmail.com>
2023-04-19 12:45:38 +0530

    Added arrays.ts, arraysExercise.ts (Completed Section 06/19 of the course)

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var activeUsers = [];
+activeUsers.push("Tony");
+var ageList = [45, 56, 13];
+ageList[0] = 99;
+// ageList[0] = "asd";
+// const bools: Array<boolean> = [];
+var bools = [];
+var coords = [];
+coords.push({ x: 23, y: 8 });
+// coords.push({ x: 23, y: "8" });
+var board = [
+    ["X", "O", "X"],
+    ["X", "O", "X"],
+    ["X", "O", "X"]
+];
