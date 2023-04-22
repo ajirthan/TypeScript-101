af16ee7d72ec556e813fafdb15558c88547f979e
Ajirthan <ajirbala28@gmail.com>
2023-04-22 19:51:58 +0530

    Added Modules(Completed Section 17/19 of the course and closing KLVR-162)

---
+++ Modules/dist/index.js
@@ -0,0 +1,6 @@
+import { add, pi, sample as randomSample } from "./utils.js";
+import { userHelper } from "./User.js";
+console.log(randomSample([12, 3, 34]));
+console.log(add(1, 2));
+console.log(pi);
+userHelper();
